0775440e4d62353411f2c93c76e9c77776177eb2
HenryChidozie <chidoziechuks@hotmail.com>
2021-12-06 17:55:40 -0500

    Adding a second route and template

--- express_server.js
+++ express_server.js
@@ -29,6 +29,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
+  res.render("url_show", templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
