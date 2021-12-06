9028ef580d533fb1eaced311ea825e62505b8251
HenryChidozie <chidoziechuks@hotmail.com>
2021-12-06 13:10:23 -0500

    Adding Routes

--- express_server.js
+++ express_server.js
@@ -8,8 +8,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get('/', (req, res) => {
-  res.send("Hello!");
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+  res.send('<h1 style = "color: green;"> Hello Justin!" </h1>');
 });
 
 app.listen(PORT, () => {
