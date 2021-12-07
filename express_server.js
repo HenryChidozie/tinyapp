1a6f43459120ca8b4ada2085e903c063b589f361
HenryChidozie <chidoziechuks@hotmail.com>
2021-12-07 18:43:12 -0500

    Updates

--- express_server.js
+++ express_server.js
@@ -1,28 +1,19 @@
-
+//REQUESTING DEPENDENCIES
 const express = require('express');
 const app = express();
 const PORT = 8080; //default port
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 //this tells the express app to use EJS as its templating engine
 app.set("view engine", "ejs");
+
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-//adding route to your express server
-app.get("/hello", (req, res) => {
-  res.json(urlDatabase);
-
-  //adding HTML tags
-  res.send('<html><body>Hello<b>Justin!"</body></html>\n');
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -30,11 +21,37 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
-  res.render("url_show", templateVars);
+  res.send('Hello!');
 });
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
+//get route to show the form
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render("urls_show", templateVars);
+});
+
+//post route to handle the form submission
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+const generateRandomString = () => {
+
+};
